7cc166988d231a3b32d68164679b23302e700a2d
sandeepmchouhan11121993 <chouhan.sandeep003@gmail.com>
2023-02-11 12:51:49 +0530

    "Variable Declaration explained"

--- VariableDeclaration.js
+++ VariableDeclaration.js
@@ -1,10 +1,19 @@
-const prefix = 'Mr.';
+/*Const is used to create coonstants whose value will not change.
+ *If we try to change it will give error.
+ */
+const prefix = "Mr.";
 console.log(prefix);
 
-let firstName="Sandeep";
+/*
+ *  let is used to create variables in ES6 javascript it's new and used now a days.
+ */
+let firstName = "Sandeep";
 console.log(firstName);
 
-var lastName ="Chouhan";
+/*
+ *  var is used to create variables in javascript it's old and not used now a days.
+ */
+var lastName = "Chouhan";
 console.log(lastName);
 
-console.log(prefix+" "+firstName+" "+lastName);
+console.log(prefix + " " + firstName + " " + lastName);
