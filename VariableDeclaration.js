2b69585449e824b5c8dbd7021149ccb1db93e47d
sandeepmchouhan11121993 <chouhan.sandeep003@gmail.com>
2023-02-11 12:51:35 +0530

    Javascript variable declaration

---
+++ VariableDeclaration.js
@@ -0,0 +1,10 @@
+const prefix = 'Mr.';
+console.log(prefix);
+
+let firstName="Sandeep";
+console.log(firstName);
+
+var lastName ="Chouhan";
+console.log(lastName);
+
+console.log(prefix+" "+firstName+" "+lastName);
