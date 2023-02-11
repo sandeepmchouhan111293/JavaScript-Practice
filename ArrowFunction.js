f07bb3b4237701038ac08023825e0e3cb6f411a1
sandeepmchouhan11121993 <chouhan.sandeep003@gmail.com>
2023-02-11 13:00:13 +0530

    "Funcion declaration"

---
+++ ArrowFunction.js
@@ -0,0 +1,16 @@
+//Normal Javascript function definition
+function printName(name){
+    console.log(name);
+  }
+  
+  //function call
+  printName("Sandeep");
+  
+  
+  //Arrow function new in javascript
+  const printLastName = (lastName) =>{
+    console.log(lastName);
+  }
+  
+  //function call
+  printLastName("Chouhan");
